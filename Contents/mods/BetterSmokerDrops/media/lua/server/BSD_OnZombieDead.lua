05cd388e9bc8ba172158fd265113f689f600bdd8
wonkotron <wonkotron@gmail.com>
2021-12-30 14:14:28 -0800

    sandbox vars refactor, new vars

--- Contents/mods/BetterSmokerDrops/media/lua/server/BSD_OnZombieDead.lua
+++ Contents/mods/BetterSmokerDrops/media/lua/server/BSD_OnZombieDead.lua
@@ -1,15 +1,23 @@
--- pull vars from global (Mod Options managed sandbox options)
-local smokerPct = BSD.OPTIONS.smokerPct
-local ignitionPct = BSD.OPTIONS.ignitionPct
+--WITH SANDBOX OPTIONS
+require('NPCs/MainCreationMethods');
+
+local function BSD_serverInit()
+    print("[BSD_serverInit] SandboxVars.BetterSmokerDrops.SmokerPercentage = ", SandboxVars.BetterSmokerDrops.SmokerPercentage);
+    print("[BSD_serverInit] SandboxVars.BetterSmokerDrops.IgnitionSourcePercentage = ", SandboxVars.BetterSmokerDrops.IgnitionSourcePercentage);
+    print("[BSD_serverInit] SandboxVars.BetterSmokerDrops.MaxCigarettesOnZombie = ", SandboxVars.BetterSmokerDrops.MaxCigarettesOnZombie);
+    print("[BSD_serverInit] SandboxVars.BetterSmokerDrops.MaxFuelInIgnitionSourcePercentage = ", SandboxVars.BetterSmokerDrops.MaxFuelInIgnitionSourcePercentage);
+end
 
 local function BSD_OnZombieDead(zombie)
     -- if zombie is a smoker
     local smokerRoll = ZombRand(101)
     -- @DEBUG:  print("[BSD] smokerRoll = ", smokerRoll)
 
-    if (smokerRoll <= smokerPct) then   
-        -- spawn between 0 and 20 cigarettes
-        local cigRoll = ZombRand(21)
+    local _temp = BSD
+
+    if (smokerRoll <= SandboxVars.BetterSmokerDrops.SmokerPercentage) then
+        -- spawn cigarettes
+        local cigRoll = ZombRand(SandboxVars.BetterSmokerDrops.MaxCigarettesOnZombie + 1)
 
         -- @DEBUG:  print("[BSD] cigRoll = ", cigRoll)
         for i=1,cigRoll do
@@ -19,14 +27,14 @@ local function BSD_OnZombieDead(zombie)
         -- if zombie has an ignition source
         local ignitionRoll = ZombRand(101)
         -- @DEBUG: print("[BSD] ignitionRoll = ", ignitionRoll)
-        if (ignitionRoll <= ignitionPct) then
+        if (ignitionRoll <= SandboxVars.BetterSmokerDrops.IgnitionSourcePercentage) then
             -- choose lighter or matches
             local ignitionSrc = "Base.Lighter"
             if (ZombRand(2) == 0) then
                 ignitionSrc = "Base.Matches"
             end
             -- spawn ignition item with random remaining durability [0.0-1.0]
-            local ignitionUse = ZombRand(101)/100
+            local ignitionUse = ZombRand(SandboxVars.BetterSmokerDrops.MaxFuelInIgnitionSourcePercentage + 1)/100
             -- @DEBUG:  print("[BSD] ignitionUse = ", ignitionUse)
 
             local ignitionItem = InventoryItemFactory.CreateItem(ignitionSrc, ignitionUse);
@@ -35,4 +43,5 @@ local function BSD_OnZombieDead(zombie)
     end
 end
 
+Events.OnGameStart.Add(BSD_serverInit)
 Events.OnZombieDead.Add(BSD_OnZombieDead)
