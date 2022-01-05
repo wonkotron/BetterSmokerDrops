54b81e60a6bbe3ae8045db488472c75636c3cff7
Tristan Ridden <hello@clockler.com>
2022-01-05 11:54:53 +1100

    Change to remove from all distributions

--- Contents/mods/BetterSmokerDrops/media/lua/server/items/BSD_Distributions.lua
+++ Contents/mods/BetterSmokerDrops/media/lua/server/items/BSD_Distributions.lua
@@ -5,22 +5,12 @@ local function BSD_onPreDistributionMerge()
     print("[BSD] removing cigarettes from zombie Distributions")
     -- need to identify base distributions: other mods sometimes end up before this in the table,
     -- and sometimes after, so no hardcoded index is correct
-    local base_dists = nil
     for k,v in pairs(Distributions) do
         if v.all and v.all.inventoryfemale and v.all.inventorymale then
-            print("  Base distributions at index " .. k)
-            base_dists = v.all
+            print("  Removing from distribution at index " .. k)
+            RemoveItemFromDistribution(v.all.inventoryfemale, "Cigarettes")
+            RemoveItemFromDistribution(v.all.inventorymale, "Cigarettes")
         end
-    end
-    if base_dists == nil then
-        print("  Didn't find base distributions")
-    else
-        -- remove cigarettes from zombie loot tables
-        local zombInv_female = base_dists["inventoryfemale"]
-        local zombInv_male = base_dists["inventorymale"]
-
-        RemoveItemFromDistribution(zombInv_female, "Cigarettes")
-        RemoveItemFromDistribution(zombInv_male, "Cigarettes")
     end
 end
 
